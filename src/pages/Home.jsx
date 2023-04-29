7ce0926eabf1886cc59c3947f027de5c1e087bbe
Jesus-0sorio <jaoz10.16@gmail.com>
2023-04-29 13:22:08 -0500

    padding buttom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Post } from '../components/Post';
 
 export const Home = () => {
 	return (
-		<div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 px-4 h-full gap-5 pt-2'>
+		<div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 px-4 h-full gap-5 pt-2 pb-12'>
 			<Post />
 			<Post />
 			<Post />
